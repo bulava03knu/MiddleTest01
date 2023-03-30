7a88244d702c9b952b33f1fd5b927f16b33f3d6e
Hennadii Bulava <bulava03@knu.ua>
2023-03-30 10:01:33 +0300

    Adding counting words function

--- main.py
+++ main.py
@@ -5,3 +5,14 @@ def read_file(file_path):
     with open(file_path, 'r') as file:
         data = file.read().replace('\n', ' ')
     return data
+
+
+def count_words(text):
+    words = re.findall(r'\b\w+\b', text.lower())
+    word_count = {}
+    for word in words:
+        if word in word_count:
+            word_count[word] += 1
+        else:
+            word_count[word] = 1
+    return word_count
